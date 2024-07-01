b6de43f30413d7f076b9525d1cedfda1f2f9333d
salmanlocofy <>
2024-07-01 19:06:33 +0000

    Updates

---
+++ _next/static/zvPbhFe7PMcdoOoKaaOQI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/2a1ad09d447557eb.css","static/chunks/pages/index-1429e2331fc0489f.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
